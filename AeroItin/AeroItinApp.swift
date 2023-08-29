d6178d196b28c8f62e429050897aa57eb237f4e0
Matt <matt.zayatz@gmail.com>
2023-08-29 11:47:56 -0400

    Initial Commit

---
+++ AeroItin/AeroItinApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroItinApp.swift
+//  AeroItin
+//
+//  Created by Matt Zayatz on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AeroItinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AeroItin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AeroItin
+//
+//  Created by Matt Zayatz on 8/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

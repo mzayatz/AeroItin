b26ded539f3ed83893b097a5e636d58094c3e144
mzayatz <matt.zayatz@gmail.com>
2023-10-23 10:48:53 -0700

    Update LineButton.swift

--- AeroItin/LineButton.swift
+++ AeroItin/LineButton.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct LineButton: View {
+    @EnvironmentObject var bidManager: BidManager
+    let line: Line
+    let action: Bidpack.TransferActions
+    
+    var buttonImage: some View {
+        switch action {
+        case .fromAvoidsToBids:
+            return Image(systemName: "plus.circle").foregroundStyle(.green)
+        case .fromAvoidsToLines:
+            return Image(systemName: "minus.circle").foregroundStyle(.gray)
+        case .fromBidsToAvoids:
+            return Image(systemName: "minus.circle").foregroundStyle(.red)
+        case .fromBidsToLines:
+            return Image(systemName: "plus.circle").foregroundStyle(.gray)
+        case .fromLinesToAvoids:
+            return Image(systemName: "minus.circle").foregroundStyle(.red)
+        case .fromLinesToBids:
+            return Image(systemName: "plus.circle").foregroundStyle(.green)
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            withAnimation {
+                bidManager.bidpack.transferLine(line: line, action: action)
+            }
+        } label: {
+            buttonImage
+        }.buttonStyle(.plain)
     }
 }
 
 #Preview {
-    LineButton()
+    var bidManager = BidManager(seat: .firstOfficer)
+    return LineButton(line: bidManager.bidpack.lines.randomElement()!, action: .fromLinesToBids)
 }

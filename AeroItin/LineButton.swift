fbadb0ee4450910db9d4d1cb60fe1bb9d503ba7d
Matt <matt.zayatz@gmail.com>
2023-10-23 09:49:45 -0700

    did a lot of things

---
+++ AeroItin/LineButton.swift
@@ -0,0 +1,18 @@
+//
+//  LineButton.swift
+//  AeroItin
+//
+//  Created by Matt Zayatz on 10/23/23.
+//
+
+import SwiftUI
+
+struct LineButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LineButton()
+}
